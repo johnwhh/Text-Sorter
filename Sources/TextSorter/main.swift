23d14cb40354e23aabeb0f7a8b1074aa6d62d3e6
johnwhh <johnwhh@icloud.com>
2020-03-30 19:30:53 -0400

    Add tests

--- Sources/TextSorter/main.swift
+++ Sources/TextSorter/main.swift
@@ -4,11 +4,19 @@ struct TextSort: ParsableCommand {
     @Argument(help: "The name of the unsorted text file.")
     var unsortedFilePath: String
     
+    @Argument(help: "The name of the sorted text file.")
+    var sortedFilePath: String?
+    
     func run() throws {
         let sorter = Sorter()
         let text = sorter.sort(path: unsortedFilePath)
         
-        print(text ?? "Unable to sort text.")
+        if sortedFilePath != nil {
+            let output = sorter.compare(text: text!, path: sortedFilePath!)
+            print(output ?? "Unable to sort text.")
+        } else {
+            print(text ?? "Unable to sort text.")
+        }
     }
 }
 
